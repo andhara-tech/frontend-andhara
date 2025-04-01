be19f1b88c566be4b71d92d5639c9484fd9cdbf8
Alejandro-REECO <revolledocoronado@hotmail.com>
2025-04-01 18:00:47 +0000

    feat(app): Translate text to spanish

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -5,7 +5,7 @@ const Page404 = () => {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-6xl font-bold">404</h1>
       <p className="mt-4 text-lg">Page Not Found</p>
-      <Link to="/">Go to the main page plase</Link>
+      <Link to="/">Regresa a la pagina principal</Link>
     </div>
   );
 };
