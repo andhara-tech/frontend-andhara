84bccf73686b1d82dd274912663b8c774efe7811
AND3SIL4 <afsilva6236@misena.edu.co>
2025-04-01 11:06:44 -0500

    test(app): package manager added "pnpm"

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Page404 = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-6xl font-bold">404</h1>
       <p className="mt-4 text-lg">Page Not Found</p>
+      <Link to="/">Go to the main page</Link>
     </div>
   );
-}
+};
 
 export default Page404;
+
