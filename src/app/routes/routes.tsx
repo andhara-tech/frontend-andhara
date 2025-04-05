5bf3b954bcd1174df5e97d41e1c97c5a530eac4a
Alejandro-RECO <revolledocoronado@hotmail.com>
2025-04-05 17:36:25 -0500

    feat(app): remove import inuse

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import DashboardPage from "../../features/dashboard/DashboardPage.tsx";
-import MarketingPage from "../../features/marketing/MarketingPage.tsx";
 import Page404 from "../../features/notFound/NotFoundPage.tsx";
 import AuthPage from "@/features/auth/AuthPage.tsx";
 
