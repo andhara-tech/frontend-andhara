71f26bf3ea17de9caf1ca93425f368bfcf2fb7b0
Alejandro-RECO <revolledocoronado@hotmail.com>
2025-04-22 15:02:17 -0500

    feat(app): Added table user and ui desing

---
+++ src/features/auth/components/UserTable.tsx
@@ -0,0 +1,51 @@
+import { useRegisterStore } from "@/app/stores/registerStore"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { useEffect } from "react"
+// import { DeleteUser } from "./deleteUser"
+
+const UserTable = () => {
+  const { getUsers, users, isLoading} = useRegisterStore()
+  useEffect(() => {
+    getUsers()
+  }
+    , [getUsers])
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  return (
+    <Table className="h-full w-full overflow-hidden">
+      <TableCaption>Lista de usuarios</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="text-left">Email</TableHead>
+          <TableHead className="text-center">Role</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {users?.map((user) => (
+          <TableRow key={user.id}>
+            <TableCell>{user.email}</TableCell>
+            <TableCell >
+              {user.role}
+            </TableCell>
+            <TableCell className="text-right">
+              {/* <DeleteUser
+                userId={user.id}
+              /> */}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  )
+}
+
+export default UserTable
--- src/features/auth/services/registerService.ts
+++ src/features/auth/services/registerService.ts
@@ -1,29 +1,38 @@
 import apiClient from "@/app/apiClient";
+import { RegisterResponse, UserRequest } from "../types/userTypes";
 
-type RegisterResponse = {
-  message: string;
-  user: {
-    email: string;
-    role: string;
-  };
-};
-
-export const registerRequest = async (
-  email: string, 
-  password: string,
-  role: string
-) => {
-  try {
-    const response = await apiClient.post<RegisterResponse>("/auth/create-user", {
-      email,
-      password,
-      role,
-    },);
-    return response;
-  } catch (error) {
-    console.error("Register failed:", error);
-    throw new Error(
-      "Register failed. Please check your credentials and try again."
-    );
+export const UserService = {
+  register: async (email: string, password: string, role: string) => {
+    try {
+      const response = await apiClient.post<RegisterResponse>("/auth/register",
+        {
+          email,
+          password,
+          role,
+        },
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error registering user:", error);
+      throw new Error("Error registering user");
+    }
+  },
+  getUsers: async () => {
+    try {
+      const response = await apiClient.get<UserRequest>(`/auth/users/`);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      throw new Error("Error fetching user");
+    }
+  },
+  deleteUser: async (userId: string) => {
+    try {
+      const response = await apiClient.delete(`/auth/delete-user/${userId}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error deleting user:", error);
+      throw new Error("Error deleting user");
+    }
   }
 }
